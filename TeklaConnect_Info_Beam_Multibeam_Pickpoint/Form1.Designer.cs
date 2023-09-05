a07a2837e117339dc37b7a9f7f5624b5e79bb390
Aakash Wani <143781221+WaniAakash@users.noreply.github.com>
2023-09-05 10:50:57 +0530

    Add files via upload

---
+++ TeklaConnect_Info_Beam_Multibeam_Pickpoint/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace TeklaConnect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.btnBeam = new System.Windows.Forms.Button();
+            this.btnBeamPP = new System.Windows.Forms.Button();
+            this.btnMBeam = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 13);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(103, 39);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(121, 39);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(25, 13);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "Info";
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Location = new System.Drawing.Point(12, 58);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(103, 39);
+            this.btnInfo.TabIndex = 2;
+            this.btnInfo.Text = "Get Info";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // btnBeam
+            // 
+            this.btnBeam.Location = new System.Drawing.Point(12, 103);
+            this.btnBeam.Name = "btnBeam";
+            this.btnBeam.Size = new System.Drawing.Size(103, 39);
+            this.btnBeam.TabIndex = 3;
+            this.btnBeam.Text = "Create Beam";
+            this.btnBeam.UseVisualStyleBackColor = true;
+            this.btnBeam.Click += new System.EventHandler(this.btnBeam_Click);
+            // 
+            // btnBeamPP
+            // 
+            this.btnBeamPP.Location = new System.Drawing.Point(12, 204);
+            this.btnBeamPP.Name = "btnBeamPP";
+            this.btnBeamPP.Size = new System.Drawing.Size(148, 39);
+            this.btnBeamPP.TabIndex = 4;
+            this.btnBeamPP.Text = "Create Beam Pick Points";
+            this.btnBeamPP.UseVisualStyleBackColor = true;
+            this.btnBeamPP.Click += new System.EventHandler(this.btnBeamPP_Click);
+            // 
+            // btnMBeam
+            // 
+            this.btnMBeam.Location = new System.Drawing.Point(12, 148);
+            this.btnMBeam.Name = "btnMBeam";
+            this.btnMBeam.Size = new System.Drawing.Size(148, 39);
+            this.btnMBeam.TabIndex = 5;
+            this.btnMBeam.Text = "Create Multiple Beam";
+            this.btnMBeam.UseVisualStyleBackColor = true;
+            this.btnMBeam.Click += new System.EventHandler(this.btnMBeam_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 292);
+            this.Controls.Add(this.btnMBeam);
+            this.Controls.Add(this.btnBeamPP);
+            this.Controls.Add(this.btnBeam);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tekla";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Button btnBeam;
+        private System.Windows.Forms.Button btnBeamPP;
+        private System.Windows.Forms.Button btnMBeam;
+    }
+}
+
---
+++ TeklaConnect_Info_Beam_Multibeam_Pickpoint/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using TSG = Tekla.Structures.Geometry3d; // TSG means alias = urf
+using TSM = Tekla.Structures.Model;
+using Tekla.Structures.Model.Geometry;
+using Tekla.Structures.Model.Operations;
+using TkUI = Tekla.Structures.Model.UI;
+using Tekla.Structures.Drawing;
+using Tekla.Structures.Model;
+
+namespace TeklaConnect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Model tkModel;
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            
+            // TriangleFunctions TF = new TriangleFunctions();
+            tkModel = new Model();
+
+            bool status = tkModel.GetConnectionStatus();
+
+            if (status == true)
+                lblInfo.Text = "Connected...!";
+
+            if (status == false)
+                lblInfo.Text = "Not Connected...!";
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            ModelInfo tkInfo = tkModel.GetInfo();
+            string sName = tkInfo.ModelName + "\nand Path is " + tkInfo.ModelPath;
+            lblInfo.Text = sName;
+        }
+
+        private void btnBeam_Click(object sender, EventArgs e)
+        {
+            TSG.Point P1 = new TSG.Point(0, 0);
+            TSG.Point P2 = new TSG.Point(3000, 0);
+
+            // TriangleFunctions TF = new TriangleFunctions(); // default constructor
+            // TriangleFunctions TF = new TriangleFunctions(12, 5); // overloaded constructor
+
+            //Beam tkBeam = new Beam(P1, P2); // call or invoke overloaded constructor
+
+            Beam tkBeam = new Beam(); // call or invoke defualt constructor
+            tkBeam.StartPoint = P1;
+            tkBeam.EndPoint = P2;
+
+            tkBeam.Material.MaterialString = "Steel_Undefined";
+            tkBeam.Profile.ProfileString = "WI300-15-20 * 300";
+            tkBeam.Class = "3";
+
+            tkBeam.Insert();
+
+            tkModel.CommitChanges();
+        }
+
+        private void btnBeamPP_Click(object sender, EventArgs e)
+        {
+            TkUI.Picker tkPicker = new TkUI.Picker();
+
+            TSG.Point P1 = tkPicker.PickPoint("Pick Start Point.");
+            TSG.Point P2 = tkPicker.PickPoint("Pick End Point.");
+
+            Beam tkBeam = new Beam(); // call or invoke defualt constructor
+            tkBeam.StartPoint = P1;
+            tkBeam.EndPoint = P2;
+
+            tkBeam.Material.MaterialString = "Steel_Undefined";
+            tkBeam.Profile.ProfileString = "WI300-15-20 * 300";
+            tkBeam.Class = "3";
+
+            tkBeam.Insert();
+
+            tkModel.CommitChanges();
+        }
+
+        private void btnMBeam_Click(object sender, EventArgs e)
+        {
+            TSG.Point P1;
+            TSG.Point P2;
+
+            double dGap = 0;
+            int n = 10;
+
+            Beam tkBeam;
+
+            for (int i = 1; i <= n; i = i + 1)
+            {
+                P1 = new TSG.Point(0, dGap);
+                P2 = new TSG.Point(3000, dGap);
+            
+                tkBeam = new Beam();
+                tkBeam.StartPoint = P1;
+                tkBeam.EndPoint = P2;
+
+                tkBeam.Material.MaterialString = "Steel_Undefined";
+                tkBeam.Profile.ProfileString = "WI300-15-20 * 300";
+                tkBeam.Class = "3";
+                
+                tkBeam.Insert();
+                tkModel.CommitChanges();
+
+                dGap = dGap + 1000;
+            }
+        }
+    }
+}
